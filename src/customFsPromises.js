de88eb0d824b7e2b1b644af44d9d2c6304bdef49
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-22 16:53:37 -0300

    feature: creating custom writeFile

---
+++ src/customFsPromises.js
@@ -0,0 +1,33 @@
+const { eventOrder } = require('./constants');
+
+class CustomFsPromises {
+    constructor({ cryptoHelper }) {
+        this.cryptoHelper = cryptoHelper;
+    }
+
+    async writeFile(filename, data, encoding = '') {
+        const encryptedText = await this.cryptoHelper.encrypt(data);
+
+        return Object.values({
+            filename,
+            encryptedText,
+            encoding
+        })
+    } 
+
+    configure() {
+        const configuration = new Map();
+
+        const writeFileOptions = {
+            when: eventOrder.beforeOriginalCall,
+            fn: this.writeFile.bind(this),
+        }
+
+        configuration.set(this.writeFile.name, writeFileOptions);
+
+        return configuration;
+    }
+
+}
+
+module.exports = { CustomFsPromises };
