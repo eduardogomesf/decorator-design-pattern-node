e81eb8d6288a81c2a6d1bc72c940670a3d653353
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-01-22 16:53:49 -0300

    feature: Creating decorator

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const app = require('./app');
 const { CryptoHelper } = require('./cryptoHelper');
+const { CustomFsPromises } = require('./customFsPromises');
 
 (async () => {
     const config = {
         cryptoKey: 'minha-senha-super-segura'
     }
     const cryptoHelper = await CryptoHelper.setup(config);
+    const customFsPromises = CustomFsPromises({ cryptoHelper }).configure();
+
 
     await app.run();
 })() 
